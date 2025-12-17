2ad4215451f0e395d968242007d37b8089f94ba1
Kirisos Guna <75839277+Kirisos-Guna@users.noreply.github.com>
2025-12-17 17:27:33 +0530

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -0,0 +1,8 @@
+# Last updated: 12/17/2025, 5:27:33 PM
+1class Solution:
+2    def getConcatenation(self, nums: List[int]) -> List[int]:
+3        nums = 2*nums
+4        return nums
+5    
+6
+7        
