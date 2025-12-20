b4080bad86521733508280ed5743e21d98652dfc
Kirisos Guna <75839277+Kirisos-Guna@users.noreply.github.com>
2025-12-20 18:25:12 +0530

    File updated at 12/20/2025, 6:25:11 PM

--- 1470-ShuffletheArray/1470-ShuffletheArray.py
+++ 1470-ShuffletheArray/1470-ShuffletheArray.py
@@ -1,22 +1,19 @@
-# Last updated: 12/20/2025, 5:47:11 PM
+# Last updated: 12/20/2025, 6:25:11 PM
 1class Solution:
-2    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-3        count = 0
-4        max_count = 0
+2    def findErrorNums(self, nums: List[int]) -> List[int]:
+3        count = {}
+4        
 5        for num in nums:
-6            if num == 1:
-7                count += 1
-8                max_count = max(max_count, count)
-9
-10            else:
-11                count = 0
-12
-13        return  max_count
-14    
-15
-16
+6            if num in count:
+7                count[num] += 1
+8            else:
+9                count[num] = 1
+10        duplicate = -1
+11        missing = -1
+12        for num in range(1, len(nums) + 1):
+13            if num not in count:
+14                missing = num
+15            elif count[num] == 2:
+16                duplicate = num
 17
-18        
-19
-20
-21      
+18        return [duplicate, missing]
